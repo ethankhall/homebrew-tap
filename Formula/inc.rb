533a795e218478977411be2fd55c877869e39a6c
Ethan Hall <ethankhall@pm.me>
2019-01-07 09:59:15 -0800

    Fixing command

--- Formula/inc.rb
+++ Formula/inc.rb
@@ -2,7 +2,7 @@ class Inc < Formula
   desc 'Include Your Builds'
   homepage "https://github.com/ethankhall/inc"
   url "https://github.com/ethankhall/inc/archive/v0.1.8.tar.gz"
-  sha265 'fcbd4b95d968a7b9b5fa64839127362a9e1559964be75ede286a53a8daf48144'
+  sha256 'fcbd4b95d968a7b9b5fa64839127362a9e1559964be75ede286a53a8daf48144'
   head "https://github.com/ethankhall/inc.git"
   
   depends_on "rust" => :build
